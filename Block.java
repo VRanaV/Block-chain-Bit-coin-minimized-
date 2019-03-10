e8a0f329cdc5a105106573cbf7d3bc1dade7c789
VRanaV <41893299+VRanaV@users.noreply.github.com>
2019-03-10 23:24:10 +0200

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,99 +1,59 @@
-import java.awt.List;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Date;
 
-
 public class Block {
-
-  private int index;
-  private long timestamp;
-  private String hash;
-  private String previousHash;
-  private String data;
-  private int nonce;
 	
-  public Block(int index, long timestamp, String previousHash, String data) {
-    this.index = index;
-    this.timestamp = timestamp;
-    this.previousHash = previousHash;
-    this.data = data;
-    nonce = 0;
-    hash = Block.calculateHash(this);
-  }
-
-  public int getIndex() {
-    return index;
-  }
-
-  public long getTimestamp() {
-    return timestamp;
-  }
-
-  public String getHash() {
-    return hash;
-  }
-
-  public String getPreviousHash() {
-    return previousHash;
-  }
-
-  public String getData() {
-    return data;
-  }
-
-  public String str() {
-    return index + timestamp + previousHash + data + nonce;
-  }
-
-  public String toString() {
-    StringBuilder builder = new StringBuilder();
-    builder.append("Block #").append(index).append(" [previousHash : ").append(previousHash).append(", ").
-    append("timestamp : ").append(new Date(timestamp)).append(", ").append("data : ").append(data).append(", ").
-    append("hash : ").append(hash).append("]");
-    return builder.toString();
-  }
+	public String hash;
+	public String previousHash; 
+	public String merkleRoot;
+	public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); //our data will be a simple message.
+	public long timeStamp; //as number of milliseconds since 1/1/1970.
+	public int nonce;
 	
-  public static String calculateHash(Block block) {
-    if (block != null) {
-      MessageDigest digest = null;
+	//Block Constructor.  
+	public Block(String previousHash ) {
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+		
+		this.hash = calculateHash(); //Making sure we do this after we set the other values.
+	}
+	
+	//Calculate new hash based on blocks contents
+	public String calculateHash() {
+		String calculatedhash = StringUtil.applySha256( 
+				previousHash +
+				Long.toString(timeStamp) +
+				Integer.toString(nonce) + 
+				merkleRoot
+				);
+		return calculatedhash;
+	}
+	
+	//Increases nonce value until hash target is reached.
+	public void mineBlock(int difficulty) {
+		merkleRoot = StringUtil.getMerkleRoot(transactions);
+		String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0" 
+		while(!hash.substring( 0, difficulty).equals(target)) {
+			nonce ++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
+	}
+	
+	//Add transactions to this block
+	public boolean addTransaction(Transaction transaction) {
+		//process transaction and check if valid, unless block is genesis block then ignore.
+		if(transaction == null) return false;		
+		if((previousHash != "0")) {
+			if((transaction.processTransaction() != true)) {
+				System.out.println("Transaction failed to process. Discarded.");
+				return false;
+			}
+		}
 
-      try {
-        digest = MessageDigest.getInstance("SHA-256");
-      } catch (NoSuchAlgorithmException e) {
-        return null;
-      }
-			
-      String txt = block.str();
-      final byte bytes[] = digest.digest(txt.getBytes());
-      final StringBuilder builder = new StringBuilder();
-			
-      for (final byte b : bytes) {
-        String hex = Integer.toHexString(0xff & b);
-
-        if (hex.length() == 1) {
-          builder.append('0');
-        }
-				
-        builder.append(hex);
-      }
-			
-      return builder.toString();
-    }
-	  
-    return null;
-  }
-  public void mineBlock(int difficulty) {
-	  nonce = 0;
-
-	  while (!getHash().substring(0,  difficulty).equals(Utils.zeros(difficulty))) {
-	    nonce++;
-	 
-	    hash = Block.calculateHash(this);
-	   // System.out.println(hash);
-	  }
-	  
-	  System.out.println("hash is  :" + hash);  
-	  }
+		transactions.add(transaction);
+		System.out.println("Transaction Successfully added to Block");
+		return true;
+	}
+	
 }
